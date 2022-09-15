fa16cc0d9edfc76a0834cae00138b73f71884793
Rodrigo Carlachiani <rodrigo@genosha.com.ar>
2022-09-15 17:31:05 -0300

    use react context

--- src/components/header/searcher.js
+++ src/components/header/searcher.js
@@ -1,13 +1,18 @@
 import { ButtonImage, Form, Input, InputButton, InputContainer, StyledH1, StyledSearcher } from "./searcherStyles";
+import { useContext } from "react";
+import { Context } from "../../context/context"
 
-function Searcher({click, change, submit, dataEntered}) {
+
+function Searcher() {
+    const context = useContext(Context)
+
     return (
         <StyledSearcher>
             <StyledH1>IP Adress Tracker</StyledH1>
             <InputContainer>
-                <Form onSubmit={submit}>
-                    <Input onChange={change} value={dataEntered || ''} placeholder="Search for any IP address" id="entered-data"/>
-                    <InputButton onClick={click}><ButtonImage src="/images/icon-arrow.svg" alt="arrow"/></InputButton>
+                <Form onSubmit={context.handleSubmit}>
+                    <Input onChange={context.handleChange} value={context.enteredData || ''} placeholder="Search for any IP address" id="entered-data"/>
+                    <InputButton onClick={context.handleClick}><ButtonImage src="/images/icon-arrow.svg" alt="arrow"/></InputButton>
                 </Form>
             </InputContainer>
         </StyledSearcher>
